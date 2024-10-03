96d13ce7bb1e67f8e1d3f91309f9f3ae954be03e
Tjommi (573433) <120416584+tjommilaskus@users.noreply.github.com>
2024-10-03 13:44:02 +0200

    Update GPSDataConverter.java

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -9,12 +9,16 @@ public class GPSDataConverter {
 	private static int TIME_STARTINDEX = 11; 
 
 	public static int toSeconds(String timestr) {
-		
+	
+		int hr = Integer.parseInt(timestr.substring(TIME_STARTINDEX, 13));
+		int min = Integer.parseInt(timestr.substring(TIME_STARTINDEX + 3, 15));
+		int sec = Integer.parseInt(timestr.substring(TIME_STARTINDEX + 6, 17));
+
 		int secs;
-		int hr, min, sec;
-		
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
+		secs = hr * 3600 + min * 60 + sec;
+
+		return secs;
+
 		
 	}
 
